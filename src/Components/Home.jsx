2aa458189494bc049a250f3cd10ad9d963a32dc2
Hindol19 <hindolbanerjee5@gmail.com>
2024-01-07 00:52:19 +0530

    added some animations

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -3,7 +3,7 @@ import "./Home.css";
 function Home() {
   return (
     <div id="Home" className="home_container">
-      <h1 className="Text">
+      <h1 className="Text" data-aos="fade-up" data-aos-duration="1000">
         DISCOVER THE TASTE OF REAL FINE DINING IN CUISINE CUINA
       </h1>
     </div>
--- src/Components/Navbar3.jsx
+++ src/Components/Navbar3.jsx
@@ -5,15 +5,21 @@ import TwitterIcon from "@mui/icons-material/Twitter";
 import FacebookIcon from "@mui/icons-material/Facebook";
 const Navbar3 = () => {
   return (
-    <>
-      <div className="nav-bg"></div>
-      <div className="nav-mid">
+    <div>
+      <div className="nav-bg" data-aos="fade-down"></div>
+      <div className="nav-mid" data-aos="fade-down" data-aos-duration="1000">
         <a className="nav-brand" href="#Home">
           Cuisine Cucina
         </a>
       </div>
       <nav className="nav-container">
-        <div className="nav-left">
+        <div
+          className="nav-left"
+          data-aos="fade-zoom-in"
+          data-aos-easing="ease-in-back"
+          data-aos-duration="1000"
+          data-aos-offset="0"
+        >
           <a href="#Home" className="nav-item left-item">
             Home
           </a>
@@ -27,13 +33,19 @@ const Navbar3 = () => {
             Contact
           </a>
         </div>
-        <div className="nav-right">
+        <div
+          className="nav-right"
+          data-aos="fade-zoom-in"
+          data-aos-easing="ease-in-back"
+          data-aos-duration="1000"
+          data-aos-offset="0"
+        >
           <InstagramIcon className="nav-item nav-icon" />
           <TwitterIcon className="nav-item nav-icon" />
           <FacebookIcon className="nav-item nav-icon" />
         </div>
       </nav>
-    </>
+    </div>
   );
 };
 
